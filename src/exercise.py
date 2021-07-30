331f07ad3ad6d1b2fcdc5f5ababa53b20cf82e03
Justin Turner <jmturner638@gmail.com>
2021-07-30 07:20:35 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #write your code below this line
+    finish = int(input("Last number?"))
+    sum = 0
+
+    for i in range(1, finish+1):
+        sum += i
+
+    print("The sum is " + str(sum))
 
 if __name__ == '__main__':
     main()
